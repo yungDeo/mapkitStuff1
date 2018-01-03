3d6a99664d2f2bdc754184d1f838a896a4554e55
MCS Devices <mcsdevices@MCSs-MacBook-Air.local>
2018-01-03 15:35:40 -0500

    Initial Commit

---
+++ mapkitStuff/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  mapkitStuff
+//
+//  Created by MCS Devices on 1/3/18.
+//  Copyright © 2018 MCS Devices. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
